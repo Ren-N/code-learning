52eee14b66022f84c46dc9051be19afa547faf05
Ren Nukada <ren@Rens-MacBook-Air13.local>
2015-03-24 15:28:26 +0900

    Server通信の実装.
    サーバーにPOST通信を行い, CGIのpythonを走らせその出力を取得. ただ普通のPostだとNS_ERROR_FAILUREが出る. MultipartでのPOSTならなぜか出ないため,そちらを採用.

--- CGI/test.py
+++ CGI/test.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python
+# coding: utf-8
+
+import cgi
+import os
+# import cgitb
+# cgitb.enable()
 
 print 'Content-Type: text/html\n\n'
 print "Hello world!"
+
+
+# GETメソッドであるか調べる
+if os.getenv('REQUEST_METHOD') == "POST" :
+	form = cgi.FieldStorage()
+	print "POST::"
+	print form.getvalue('code', 'none')
+	print form.getvalue('lang', 'none')
+
+	if form.haskey("code"):
+		print "haskey"
+	else:
+		print "nokey"
+	print "aa"
+	print form["code"].value
+
--- assets/js/editor.js
+++ assets/js/editor.js
@@ -11,7 +11,7 @@ function init(){
     document.getElementById( 'editor' ).style.fontSize = '14';
     editor.getSession().on('change', function(){
     	// 更新時に現在の文字をtextariaに避難.
-        var textcode = editor.getValue();
+        var textcode = editor.getSession().getValue();
         document.getElementById( 'text' ).value = textcode;
     });
     // --------------------------------
@@ -118,28 +118,72 @@ function setTemplate(){
 // 「コンパイル」ボタンを押したとき実行
 function compile(){
 	startCompileAnimation();
-	//サーバーに送信.
+	var selecter = document.getElementById('selectedLang');
+	var lang = selecter.options[selecter.selectedIndex].value;
+	var data = { 
+		lang : lang,
+		code : editor.getValue() 
+	};
 	var url = "CGI/test.py";
-	var request =  new XMLHttpRequest();;//createXMLHttpRequest();
+	multiPost(url,data);
+	
+}
+
+function multiPost(url,data){
+	//サーバーに送信.
+	var request =  new XMLHttpRequest();
 	request.open("POST", url, true);
-	// request.setRequestHeader("Access-Control-Allow-Origin","*");
-	request.send(editor.getValue());
+	//form-data,POST送信
+	boundary = "-----";
+    request.setRequestHeader("content-type",
+                         "multipart/form-data; boundary="+boundary);
+    var senddata = "";
+    for (name in data) {
+      senddata += "--"+boundary+"\r\n"+
+        "Content-Disposition: form-data; name=\""+name+"\"\r\n\r\n"+
+        data[name]+"\r\n";
+    }
+    senddata += "--"+boundary+"--\r\n";
 	//ハンドラ.
 	request.onreadystatechange = function() {
 		if (request.readyState == 4 && request.status == 200) {
 			//受信完了時の処理.
-			var selecter = document.getElementById('selectedLang');
-			var lang = selecter.options[selecter.selectedIndex].value;
-			var exec = "run code."+lang;
+			var exec = "run code."+data['lang'];
 			var header = "$ " + exec + "<br><br>"
 			var terminal = document.getElementById('csl-box');
 			terminal.innerHTML = header + decodeURI(request.responseText).replace(/\r?\n/g, '<br>');
 			stopCompileAnimation();
 		}
 	}
+    request.send(senddata);  
 }
 
-
+function post(url,data){
+	//サーバーに送信.
+	var request =  new XMLHttpRequest();
+	request.open("POST", url, true);
+	//form-data,POST送信
+    request.setRequestHeader("content-type","application/x-www-form-urlencoded;charset=UTF-8");
+    var senddata = "";
+    for (name in data) {
+      if (senddata != "")
+        senddata += "&";
+      senddata += name+"="+escape(data[name]);
+    }
+    request.send(senddata); 
+	//ハンドラ.
+	request.onreadystatechange = function() {
+		if (request.readyState == 4 && request.status == 200) {
+			//受信完了時の処理.
+			var exec = "run code."+data['lang'];
+			var header = "$ " + exec + "<br><br>"
+			var terminal = document.getElementById('csl-box');
+			terminal.innerHTML = header + decodeURI(request.responseText).replace(/\r?\n/g, '<br>');
+			stopCompileAnimation();
+		}
+	}
+    request.send(senddata);  
+}
 
 
 function startCompileAnimation(){
@@ -161,9 +205,5 @@ function changeLang(){
 function setTabName (name) {
 	document.getElementById('tab-filename').innerHTML = name;
 }
-
-
-
-
 
 
